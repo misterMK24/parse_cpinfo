c45a1f72f4de8ce503806c3cb05e41092d7e0881
misterMK24 <41155221+misterMK24@users.noreply.github.com>
2018-11-29 11:32:54 +0300

    writinig_to_file

--- main_func.py
+++ main_func.py
@@ -5,7 +5,7 @@ import sys
 import codecs
 
 def main_func():
-    with codecs.open("C:\_python\objects_5_0.C", "r", "utf-8", errors='ignore') as file:
+    with codecs.open("C:\_python\\attestaciya\\objects_5_0.C", "r", "utf-8", errors='ignore') as file:
         result_str = []
         index = 0
         dict_parse = dict.fromkeys('name', 'ipaddr')
@@ -17,17 +17,18 @@ def main_func():
                 if name:
                     index = 2
                     name_parse = re.findall(r'\b\S+\b', name[0])
-                    print(name_parse[1])
+                    # print(name_parse[1])
                     # result_str.append(name[0])
             elif index == 2:
                 ip_addr = re.findall(r'\s+:ipaddr\s\(\S+\)', line)
                 if ip_addr:
                     index = 0
                     ip_addr_parse = re.findall(r'\b\S+\b', ip_addr[0])
-                    print(ip_addr_parse[1])
-                    # result_str.append(ip_addr[0])
-                    # print(result_str)
-
+                    # print(ip_addr_parse[1])
+                    result_str.append(name_parse[1] + "   " + ip_addr_parse[1])
+    with codecs.open("C:\_python\\results\\results_parse.txt", "a+", "cp1251", errors='ignore') as file:
+        for line in result_str:
+            file.write(line + "\n")
 
 
 main_func()
