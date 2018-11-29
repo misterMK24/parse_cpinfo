7475aa2147363000eb730831b9ca49e2fa8ccaee
misterMK24 <41155221+misterMK24@users.noreply.github.com>
2018-11-29 15:18:31 +0300

    end_with_group_obj

--- main_func.py
+++ main_func.py
@@ -4,11 +4,18 @@ import json
 import sys
 import codecs
 
-def main_func():
+
+
+def compare_str():
+    hosts = host_plain()
+    groups = group_obj()
+    
+
+def host_plain():
     with codecs.open("C:\_python\\attestaciya\\objects_5_0.C", "r", "utf-8", errors='ignore') as file:
         result_str = []
         index = 0
-        dict_parse = dict.fromkeys('name', 'ipaddr')
+        # dict_parse = dict.fromkeys('name', 'ipaddr')
         for line in file:
             if re.findall(r'\s+:ClassName\s\(host_plain\)', line):
                 index = 1
@@ -29,6 +36,39 @@ def main_func():
     with codecs.open("C:\_python\\results\\results_parse.txt", "a+", "cp1251", errors='ignore') as file:
         for line in result_str:
             file.write(line + "\n")
+    return result_str
 
+def group_obj():
+    with codecs.open("C:\_python\\attestaciya\\objects_5_0.C", "r", "utf-8", errors='ignore') as file:
+        result_str = []
+        index = 0
+        dict_parse = dict.fromkeys(['name', 'ref_obj'])
+        ref_obj = None
+        for line in file:
+            if re.findall(r'\s+:ClassName\s\(network_object_group\)', line):
+                index = 1
+            elif index == 1:
+                name = re.findall(r'\s+:name\s\(\S+\)', line)
+                if name:
+                    index = 2
+                    name_parse = re.findall(r'\b\S+\b', name[0])
+                    result_str.append("Group: " + name_parse[1])
+                    # dict_parse['name'] = name_parse[1]
+            elif index == 2:
+                if re.findall(r'\s+:color', line):
+                    index = 0
+                elif re.findall(r'\s+:\s\(ReferenceObject', line):
+                    ref_obj = 1
+                elif ref_obj:
+                        if re.findall(r'\s+:Name\s\(\S+\)', line):
+                            ref_obj_parse = re.findall(r'\b\S+\b', line)
+                            if ref_obj_parse:
+                                result_str.append(ref_obj_parse[1])
+                                # dict_parse['ref_obj'] = ref_obj_parse[1]
+        with codecs.open("C:\_python\\results\\results_parse_groups.txt", "a+", "cp1251", errors='ignore') as file:
+            for line in result_str:
+                file.write(line + "\n")
+        return result_str
 
-main_func()
+# group_obj()
+# host_plain()
