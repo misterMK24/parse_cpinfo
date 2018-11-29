72c63f576c941212c5664663db4b166e2339e7b5
misterMK24 <41155221+misterMK24@users.noreply.github.com>
2018-11-29 17:11:22 +0300

    added_compare_str
    there is an issue with re in droups_obj and with dict in addition

--- main_func.py
+++ main_func.py
@@ -9,13 +9,23 @@ import codecs
 def compare_str():
     hosts = host_plain()
     groups = group_obj()
-    
+    try:
+        for key, value in hosts.items():
+            for key_1 in groups:
+                if key == key_1:
+                    groups.update({key: value})
+    except ValueError:
+        pass
+    print(group for group in groups)
+    with open("C:\\_python\\results\\results_finish.txt", 'a+') as file:
+        for hostname, ipaddr in groups.items():
+            file.write("{} {}\n".format(hostname, ipaddr))
 
 def host_plain():
     with codecs.open("C:\_python\\attestaciya\\objects_5_0.C", "r", "utf-8", errors='ignore') as file:
         result_str = []
         index = 0
-        # dict_parse = dict.fromkeys('name', 'ipaddr')
+        dict_parse = {}
         for line in file:
             if re.findall(r'\s+:ClassName\s\(host_plain\)', line):
                 index = 1
@@ -24,25 +34,23 @@ def host_plain():
                 if name:
                     index = 2
                     name_parse = re.findall(r'\b\S+\b', name[0])
-                    # print(name_parse[1])
-                    # result_str.append(name[0])
             elif index == 2:
                 ip_addr = re.findall(r'\s+:ipaddr\s\(\S+\)', line)
                 if ip_addr:
                     index = 0
                     ip_addr_parse = re.findall(r'\b\S+\b', ip_addr[0])
-                    # print(ip_addr_parse[1])
-                    result_str.append(name_parse[1] + "   " + ip_addr_parse[1])
+                    dict_parse[name_parse[1]] = ip_addr_parse[1]
+                    # result_str.append(name_parse[1] + "   " + ip_addr_parse[1])
     with codecs.open("C:\_python\\results\\results_parse.txt", "a+", "cp1251", errors='ignore') as file:
         for line in result_str:
             file.write(line + "\n")
-    return result_str
+    return dict_parse
 
 def group_obj():
     with codecs.open("C:\_python\\attestaciya\\objects_5_0.C", "r", "utf-8", errors='ignore') as file:
         result_str = []
         index = 0
-        dict_parse = dict.fromkeys(['name', 'ref_obj'])
+        dict_name = {}
         ref_obj = None
         for line in file:
             if re.findall(r'\s+:ClassName\s\(network_object_group\)', line):
@@ -52,8 +60,9 @@ def group_obj():
                 if name:
                     index = 2
                     name_parse = re.findall(r'\b\S+\b', name[0])
-                    result_str.append("Group: " + name_parse[1])
-                    # dict_parse['name'] = name_parse[1]
+                    name_parse[1] = "\n" + "Group:  " + name_parse[1] + "\n"
+                    result_str.append(name_parse[1])
+                    dict_name[name_parse[1]] = " "
             elif index == 2:
                 if re.findall(r'\s+:color', line):
                     index = 0
@@ -63,12 +72,15 @@ def group_obj():
                         if re.findall(r'\s+:Name\s\(\S+\)', line):
                             ref_obj_parse = re.findall(r'\b\S+\b', line)
                             if ref_obj_parse:
+                                dict_name[ref_obj_parse[1]] = ""
+                                ref_obj = None
                                 result_str.append(ref_obj_parse[1])
-                                # dict_parse['ref_obj'] = ref_obj_parse[1]
         with codecs.open("C:\_python\\results\\results_parse_groups.txt", "a+", "cp1251", errors='ignore') as file:
             for line in result_str:
                 file.write(line + "\n")
-        return result_str
+        return dict_name
 
+
+compare_str()
 # group_obj()
 # host_plain()
